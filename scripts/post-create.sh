05c82cc386c86ba8b34d5f6c206b67991f288865
Johan Martin <jnm+johanmartindev@catenare.com>
2023-10-27 14:39:16 +0200

    Update post-create script

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -4,3 +4,4 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 git config --global --add safe.directory /workspace
 git config --global user.name "Johan Martin"
 git config --global user.email "jnm+johanmartindev@catenare.com"
+git config --global --unset gpg.ssh.program
