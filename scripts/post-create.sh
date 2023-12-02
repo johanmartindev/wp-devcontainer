ce74335a477b9077c50d2cb389c0b137cf10dde6
Johan Martin <jnm+johanmartindev@catenare.com>
2023-12-02 14:30:33 +0000

    Fix issue with startup script

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -3,4 +3,5 @@ set -xe
 git config --global --add safe.directory /workspace
 git config --global user.name "Johan Martin"
 git config --global user.email "jnm+johanmartindev@catenare.com"
+git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY" && git config commit.gpgsign true --global
 git config --global --unset gpg.ssh.program
