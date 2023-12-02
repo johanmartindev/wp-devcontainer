2da75c0c63ac6954fadec179af52e81fbcc841ce
Johan Martin <jnm+johanmartindev@catenare.com>
2023-12-02 14:23:57 +0000

    Fix issue with startup script

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -xe
 git config --global --add safe.directory /workspace
-/workspace/wordpress-development/wp-site-setup.sh
 git config --global user.name "Johan Martin"
 git config --global user.email "jnm+johanmartindev@catenare.com"
 git config --global --unset gpg.ssh.program
