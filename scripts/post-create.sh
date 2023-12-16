e9868c74998693425e2325c9ebc0a2a99c92a6e2
Johan Martin <jnm+johanmartindev@catenare.com>
2023-12-16 16:50:17 +0200

    Cleanup code

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -xe
-python3 -m venv /home/$USERNAME/ansible
-echo "PATH=$PATH:/home/$USERNAME/ansible/bin" >> /home/$USERNAME/.zshrc
-source /home/$USERNAME/.zshrc
-/home/$USERNAME/ansible/bin/python /home/$USERNAME/ansible/bin/pip install ansible
-ansible --version
 git config --global --add safe.directory /workspace
 git config --global user.name "Johan Martin"
 git config --global user.email "jnm+johanmartindev@catenare.com"
