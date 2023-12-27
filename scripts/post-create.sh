5b73ed96f0321f039dd0ce413f58aedb8070872f
Johan Martin <jnm+johanmartindev@catenare.com>
2023-12-27 08:32:40 +0000

    Reset scripts

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
-echo $USERNAME
-python3 -m venv /home/$USERNAME/ansible
-echo "PATH=$PATH:/home/$USERNAME/ansible/bin" >> /home/$USERNAME/.zshrc
-/home/$USERNAME/ansible/bin/python /home/$USERNAME/ansible/bin/pip install ansible
-ansible --version
-/home/vscode/ansible/bin/pip3 install ansible-lint
-source /home/$USERNAME/.zshrc
+set -xe
+git config --global pull.rebase true
+git config --global --add safe.directory /workspace
+git config --global user.name "Johan Martin"
+git config --global user.email "jnm+johanmartindev@catenare.com"
+git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY"
+git config --global commit.gpgsign true
+git config --global --unset gpg.ssh.program
--- scripts/post-start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env zsh
-set -xe
-git config --global --add safe.directory /workspace
-git config --global user.name "Johan Martin"
-git config --global user.email "jnm+johanmartindev@catenare.com"
-git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY"
-git config --global commit.gpgsign true
-git config --global --unset gpg.ssh.program
