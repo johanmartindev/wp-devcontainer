81bd18ef2468d5b4e18cc7ea85a57b6c75e11f9a
Johan Martin <jnm+johanmartindev@catenare.com>
2024-02-14 07:59:57 +0000

    Update script for setup

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -5,4 +5,8 @@ git config --global user.name "Johan Martin"
 git config --global user.email "jnm+johanmartindev@catenare.com"
 git config --global gpg.format ssh && git config --global user.signingkey "$SSH_SIGNING_KEY"
 git config --global commit.gpgsign true
-git config --global --unset gpg.ssh.program
+if git config --global gpg.ssh.program; then
+  echo "Unsetting gpg.ssh.program"
+  git config --global --unset gpg.ssh.program
+fi
+exit
